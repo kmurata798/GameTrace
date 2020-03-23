30cd76ac0009429ab63e85fdd3707596782d6d67
kmurata798 <kmurata798@gmail.com>
2020-03-23 14:37:18 -0700

    [Add] User account and picture update. Deploying to heroku

--- Project/gametrace/forms.py
+++ Project/gametrace/forms.py
@@ -1,4 +1,7 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+# FileField == type of field it is, FileAllowed == what kind of files we want uploaded validator
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 # These imports allow for the creation of forms for the site (Login/Sign up)
@@ -34,3 +37,25 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            # if user already exists, then raise error. We don't want duplicate users!
+            if user:
+                raise ValidationError('That username is already taken. Please try another username.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            # if user already exists, then raise error. We don't want duplicate users!
+            if user:
+                raise ValidationError('That email is already taken. Please try another email.')
