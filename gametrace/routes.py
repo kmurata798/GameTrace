fd4c6c53789260cfb4c3428a8439ecc248be2c35
kmurata798 <kmurata798@gmail.com>
2020-03-24 00:21:59 -0700

    [Add] Code snippet at top of route.py to allow me to modify css without having to clear browser cache everytime in order to see the changes made

--- gametrace/routes.py
+++ gametrace/routes.py
@@ -7,6 +7,20 @@ from gametrace.forms import RegistrationForm, LoginForm, UpdateAccountForm, Post
 from gametrace.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 
+""" Code Snippet used ==> Lets me modify my .css file and refresh the website without having to 
+ repeatedly clear the browser cache EVERYTIME! :D """
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 # Unique=True means that only one user can own the specific password, username, etc..
 # nullable=True means that the specified field can be empty/none/null
