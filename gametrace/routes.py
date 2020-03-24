f6e0f157fc01be5b3bb1124d227f9a43ea7bd2cd
kmurata798 <kmurata798@gmail.com>
2020-03-23 22:30:18 -0700

    [Add] Update button-link + Delete Modal functional

--- gametrace/routes.py
+++ gametrace/routes.py
@@ -140,13 +140,16 @@ def update_post(post_id):
     return render_template('create_post.html', title='Update Post',
                            form=form, legend='Update Post')
 
-# @app.route("/post/<int:post_id>/delete", methods=['POST'])
-# @login_required
-# def delete_post(post_id):
-#     post = Post.query.get_or_404(post_id)
-#     if post.author != current_user:
-#         abort(403)
-#     db.session.delete(post)
-#     db.session.commit()
-#     flash('Your post has been deleted!', 'success')
-#     return redirect(url_for('home'))
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+# Only take POST, since we only accept when user submits DELETE from MODAL
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    # Delete post from database
+    db.session.commit()
+    # Commit changes to database
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
